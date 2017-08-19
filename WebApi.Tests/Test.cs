6560b65641913a0b5959689dffe9ad1d70893282
Andrei Git <Andrei@andreis-mbp.home>
2017-08-19 22:13:21 +0200

    init

---
+++ WebApi.Tests/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace WebApi.Tests
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestCase()
+        {
+        }
+    }
+}
---
+++ WebApi/Controllers/CommunicationController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    [RoutePrefix("api/communication")]
+    public class CommunicationController : ApiController
+    {
+        public CommunicationController()
+        {
+        }
+
+        [HttpGet]
+        [Route("getData")]
+        public string GetData(){
+            return "hello world";
+        }
+    }
+}
